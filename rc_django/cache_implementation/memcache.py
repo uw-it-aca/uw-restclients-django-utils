e30d1689d12f23422b4b6b12dc3c8711791a79e3
Fang Lin <fanglin@uw.edu>
2018-08-27 16:24:49 -0700

    Fix python 3 TypeError

--- rc_django/cache_implementation/memcache.py
+++ rc_django/cache_implementation/memcache.py
@@ -8,8 +8,10 @@ from bmemcached.exceptions import MemcachedException
 import json
 import logging
 import threading
+from dateutil.parser import parse
 from django.conf import settings
 from django.utils import timezone
+
 from restclients_core.models import MockHTTP
 
 
@@ -18,46 +20,50 @@ logger = logging.getLogger(__name__)
 
 class MemcachedCache(object):
 
+    def __init__(self):
+        self._set_client()
+
     def getCache(self, service, url, headers):
-        client = self._get_client()
         key = self._get_key(service, url)
         try:
-            data = client.get(key)
+            data = self.client.get(key)
         except MemcachedException as ex:
             logger.error("Get (key: %s) ==> %s", key, str(ex))
             return
 
         if not data:
-            return
+            return None
 
         values = json.loads(data)
-        if "b64_data" in data:
-            values["data"] = b64decode(values["b64_data"])
         response = MockHTTP()
-        response.status = values["status"]
-        response.data = values["data"]
         response.headers = values["headers"]
+        response.status = values["status"]
+
+        if "b64_data" in values:
+            response.data = b64decode(values["b64_data"])
+        else:
+            response.data = values["data"]
 
         return {"response": response}
 
     def updateCache(self, service, url, new_data, new_data_dt):
         """
         :param new_data: a string representation of the data
-        :param new_data_dt: a timezone aware datetime object
+        :param new_data_dt: a timezone aware datetime object giving
+                the timestamp of the new_data
         :raise MemcachedException: if update failed
         """
-        client = self._get_client()
         key = self._get_key(service, url)
 
         # clear existing data
         try:
-            value = client.get(key)
+            value = self.client.get(key)
             if value:
                 data = json.loads(value)
                 if "time_stamp" in data:
                     cached_data_dt = parse(data["time_stamp"])
                     if new_data_dt > cached_data_dt:
-                        client.delete(key)
+                        self.client.delete(key)
                         # may raise MemcachedException
                         logger.info("IN cache (key: %s), older DELETE", key)
                     else:
@@ -71,23 +77,27 @@ class MemcachedCache(object):
             return
 
         # store new value in cache
-        cdata, time_to_store = self.__make_cache_data(
+        cdata, time_to_store = self._make_cache_data(
             service, url, new_data, {}, 200, new_data_dt)
 
-        client.set(key, cdata, time=time_to_store)
+        self.client.set(key, cdata, time=time_to_store)
         # may raise MemcachedException
         logger.info("MemCached SET (key %s) for %d seconds",
                     key, time_to_store)
 
-    def __make_cache_data(self, service, url, value_str,
-                          header_data, status, time_stamp):
-        b64_data = b64encode(value_str)
-        data = json.dumps({"status": status,
-                           "b64_data": b64_data,
-                           "headers": header_data,
-                           "time_stamp": str(time_stamp)})
+    def _make_cache_data(self, service, url, data_to_cache,
+                         header_data, status, time_stamp):
+        data = {"status": status,
+                "headers": header_data,
+                "time_stamp": time_stamp.isoformat()
+                }
+        if isinstance(data_to_cache, (bytes, bytearray)):
+            data["b64_data"] = b64encode(data_to_cache)
+        else:
+            data["data"] = data_to_cache
+
         time_to_store = self.get_cache_expiration_time(service, url)
-        return data, time_to_store
+        return json.dumps(data), time_to_store
 
     def processResponse(self, service, url, response):
         header_data = {}
@@ -95,14 +105,13 @@ class MemcachedCache(object):
             header_data[header] = response.getheader(header)
 
         key = self._get_key(service, url)
-        cdata, time_to_store = self.__make_cache_data(service, url,
-                                                      response.data,
-                                                      header_data,
-                                                      response.status,
-                                                      timezone.now())
-        client = self._get_client()
+        cdata, time_to_store = self._make_cache_data(service, url,
+                                                     response.data,
+                                                     header_data,
+                                                     response.status,
+                                                     timezone.now())
         try:
-            client.set(key, cdata, time=time_to_store)
+            self.client.set(key, cdata, time=time_to_store)
             logger.info("MemCached set with key '%s', %d seconds",
                         key, time_to_store)
         except bmemcached.exceptions.MemcachedException as ex:
@@ -116,20 +125,18 @@ class MemcachedCache(object):
     def _get_key(self, service, url):
         return "%s-%s" % (service, url)
 
-    def _get_client(self):
+    def _set_client(self):
         thread_id = threading.current_thread().ident
         if not hasattr(MemcachedCache, "_memcached_cache"):
             MemcachedCache._memcached_cache = {}
 
         if thread_id in MemcachedCache._memcached_cache:
-            return MemcachedCache._memcached_cache[thread_id]
+            self.client = MemcachedCache._memcached_cache[thread_id]
+            return
 
         servers = settings.RESTCLIENTS_MEMCACHED_SERVERS
         username = getattr(settings, "RESTCLIENTS_MEMCACHED_USER", None)
         password = getattr(settings, "RESTCLIENTS_MEMCACHED_PASS", None)
 
-        client = bmemcached.Client(servers, username, password)
-
-        MemcachedCache._memcached_cache[thread_id] = client
-
-        return client
+        self.client = bmemcached.Client(servers, username, password)
+        MemcachedCache._memcached_cache[thread_id] = self.client
--- rc_django/tests/cache_implementation/test_memcached.py
+++ rc_django/tests/cache_implementation/test_memcached.py
@@ -1,106 +1,67 @@
 import json
 from django.test import TestCase
-from django.conf import settings
 from django.utils import timezone
-from restclients_core.dao import DAO, MockDAO
-from restclients_core.models import MockHTTP
 from rc_django.cache_implementation.memcache import MemcachedCache
-from unittest import skipIf
 
 
-MEMCACHE = 'rc_django.cache_implementation.memcache.MemcachedCache'
+class MClient():
+
+    cache = {}
+
+    def delete(self, key):
+        if key in MClient.cache:
+            del MClient.cache[key]
+
+    def get(self, key):
+        if key in MClient.cache:
+            return MClient.cache[key]
+        return None
+
+    def set(self, key, cdata, time=None):
+        if key not in MClient.cache:
+            MClient.cache[key] = cdata
 
 
-class MEM_DAO(DAO):
-    def service_name(self):
-        return "mem"
+class TestCache(MemcachedCache):
 
-    def get_default_service_setting(self, key):
-        if "DAO_CLASS" == key:
-            return (
-                "rc_django.tests.cache_implementation.test_memcached.Backend")
+    def __init__(self):
+        super(TestCache, self).__init__()
 
-    def get_cache(self):
-        return MemcachedCache()
+    def _set_client(self):
+        self.client = MClient()
 
-
-class Backend(MockDAO):
-    def load(self, method, url, headers, body):
-        response = MockHTTP()
-        if url == "/same":
-            response.status = 200
-            response.data = "Body Content"
-        else:
-            response.status = 404
-        return response
+    def make_cache_data(self):
+        return self._make_cache_data(
+            'mem', '/same', json.dumps({"data": "Body Content"}),
+            {}, 200, timezone.now())
 
 
 class MemcachedCacheTest(TestCase):
-    @skipIf(not getattr(settings, 'RESTCLIENTS_TEST_MEMCACHED', False),
-            "Needs configuration to test memcached cache")
-    def test_memcached(self):
-        cache = MemcachedCache()
-        client = MEM_DAO()
-        client.getURL('/same', {})
+
+    def test_cacheGet(self):
+        cache = TestCache()
+        key = cache._get_key('mem', '/same')
+        self.assertEquals(key, "mem-/same")
+
+        data = cache.getCache('mem', '/same', {})
+        self.assertIsNone(data)
+
+        cdata, time_to_store = cache.make_cache_data()
+        cache.client.set(key, cdata, time_to_store)
 
         hit = cache.getCache('mem', '/same', {})
         response = hit["response"]
-
         self.assertEquals(response.status, 200)
-        self.assertEquals(response.data, "Body Content")
-
-        hit = cache.getCache('mem', '/same', {})
-
-        self.assertEquals(response.status, 200)
-        self.assertEquals(response.data, "Body Content")
-
-    @skipIf(not getattr(settings, 'RESTCLIENTS_TEST_MEMCACHED', False),
-            "Needs configuration to test memcached cache")
-    def test_memcached_404(self):
-        cache = MemcachedCache()
-        client = MEM_DAO()
-        client.getURL('/asd', {})
-
-        hit = cache.getCache('mem', '/asd', {})
-        response = hit["response"]
-        self.assertEquals(response.status, 404)
-
-        hit = cache.getCache('mem', '/asd', {})
-        self.assertEquals(response.status, 404)
-
-    @skipIf(not getattr(settings, 'RESTCLIENTS_TEST_MEMCACHED', False),
-            "Needs configuration to test memcached cache")
-    def test_longkeys(self):
-        cache = MemcachedCache()
-        url = "".join("X" for i in range(300))
-
-        ok_response = MockHTTP()
-        ok_response.status = 200
-        ok_response.data = "valid"
-        cache.processResponse('ok', url, ok_response)
-
-        # This makes sure we don't actually cache anything when the url
-        # is too long
-        response = cache.getCache('ok', url, {})
-        self.assertEquals(response, None)
-
-        # But the get doesn't raise the exception w/ the set before it,
-        # so this redundant-looking code is testing to make sure that we
-        # catch the exception on the get as well.
-        url = "".join("Y" for i in range(300))
-        response = cache.getCache('ok', url, {})
-        self.assertEquals(response, None)
+        self.assertEquals(response.data, '{"data": "Body Content"}')
 
     def test_updateCache(self):
-        with self.settings(
-                RESTCLIENTS_DAO_CACHE_CLASS=MEMCACHE,
-                RESTCLIENTS_TEST_MEMCACHED=True,
-                RESTCLIENTS_MEMCACHED_SERVERS=('localhost:11211', )):
-            cache = MemcachedCache()
-            c_entry = cache.getCache('mem', '/same', {})
-            self.assertIsNone(c_entry)
+        cache = TestCache()
+        cache.updateCache('mem', '/same',
+                          '{"data": "Body Content"}',
+                          timezone.now())
 
-            c_entry = cache.updateCache('mem', '/same',
-                                        json.dumps({"Updared": True}),
-                                        timezone.now())
-            c_entry = cache.getCache('mem', '/same', {})
+        hit = cache.getCache('mem', '/same', {})
+        response = hit["response"]
+        self.assertEquals(response.headers, {})
+        self.assertEquals(response.status, 200)
+        self.assertEquals(response.data, '{"data": "Body Content"}')
