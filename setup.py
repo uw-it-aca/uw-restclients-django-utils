4a6d7b541752ba659961e93dc2b023a95bb4d79e
Jim Laney <jlaney@uw.edu>
2018-10-23 11:04:41 -0700

    >= django2.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=[
-        'Django>2.1,<3.0',
+        'Django>=2.1,<3.0',
         'UW-RestClients-Core>1.0,<2.0',
         'django-userservice>3.1,<4.0',
         'python-binary-memcached',
